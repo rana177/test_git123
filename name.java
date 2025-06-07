edfc588a1df41a7a05f708a850eb5f2e97f9168d
rana <deyrana2016@gmail.com>
2025-06-08 02:45:43 +0530

    nuob

--- name.java
+++ name.java
@@ -1,5 +1,5 @@
 public class PrintName {
     public static void main(String[] args) {
-        System.out.println("ritesh"); // Replace "Your Name" with your actual name
+        System.out.println("Updated"); // Replace "Your Name" with your actual name
     }
 }
