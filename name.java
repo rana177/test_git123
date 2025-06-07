630b3e776237d45fd7d21fa2c3516ac291269215
rana <deyrana2016@gmail.com>
2025-06-08 02:13:01 +0530

    modified

--- name.java
+++ name.java
@@ -1,5 +1,5 @@
 public class PrintName {
     public static void main(String[] args) {
-        System.out.println("RANA Dey"); // Replace "Your Name" with your actual name
+        System.out.println("ritesh"); // Replace "Your Name" with your actual name
     }
 }
